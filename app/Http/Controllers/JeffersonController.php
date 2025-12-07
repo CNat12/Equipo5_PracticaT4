dd0045e8d39f410ed801f13ce878311188c036b5
AngelJeffersonJ <almno.angel.jimeneza@ceytea.edu.mx>
2025-12-07 01:14:01 -0600

    Add JeffersonController and corresponding unit tests for security level analysis

---
+++ app/Http/Controllers/JeffersonController.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+/**
+ * Class JeffersonController
+ */
+class JeffersonController extends Controller
+{
+    /**
+     * @return array<string, mixed>
+     */
+    public function analyzeSecurity(Request $request): array
+    {
+        $level = strtolower($request->query('level', 'medium'));
+
+        $securityMatrix = [
+            'low' => [
+                "X-Content-Type-Options"
+            ],
+            'medium' => [
+                "X-Content-Type-Options",
+                "X-XSS-Protection"
+            ],
+            'high' => [
+                "X-Content-Type-Options",
+                "X-XSS-Protection",
+                "Strict-Transport-Security",
+                "X-Frame-Options",
+                "Referrer-Policy"
+            ]
+        ];
+
+        if (!array_key_exists($level, $securityMatrix)) {
+            return [
+                "requested_level" => $level,
+                "valid_level" => false,
+                "message" => "Security level not recognized"
+            ];
+        }
+
+        $presentHeadersMock = [
+            "X-Content-Type-Options",
+            "X-XSS-Protection"
+        ];
+
+        $missing = array_diff($securityMatrix[$level], $presentHeadersMock);
+
+        $score = (int) round(
+            (count($securityMatrix[$level]) - count($missing)) /
+            count($securityMatrix[$level]) * 100
+        );
+
+        return [
+            "requested_level" => $level,
+            "valid_level" => true,
+            "required_headers" => $securityMatrix[$level],
+            "present_headers" => $presentHeadersMock,
+            "missing_headers" => array_values($missing),
+            "score" => $score
+        ];
+    }
+}
