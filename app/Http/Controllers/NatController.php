34e5f855f1488cf680d477c793e34fb8ecf33df2
CNat12 <diazdeleondolores12@gmail.com>
2025-12-06 22:30:45 -0600

    :star: Normal sin error

--- app/Http/Controllers/NatController.php
+++ app/Http/Controllers/NatController.php
@@ -5,7 +5,7 @@ namespace App\Http\Controllers;
 use Illuminate\Http\JsonResponse;
 use Illuminate\Http\Request;
 
-class NatController extends C
+class NatController extends Controller
 {
     public function check(Request $request): JsonResponse
     {
