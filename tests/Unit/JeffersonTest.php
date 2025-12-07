b78f4b2e60c02a7875b118eabcca7a64a83cb88f
AngelJeffersonJ <almno.angel.jimeneza@ceytea.edu.mx>
2025-12-07 01:23:18 -0600

    :art: Reorder use statements in JeffersonTest for improved organization

--- tests/Unit/JeffersonTest.php
+++ tests/Unit/JeffersonTest.php
@@ -2,9 +2,9 @@
 
 namespace Tests\Unit;
 
-use PHPUnit\Framework\TestCase;
-use Illuminate\Http\Request;
 use App\Http\Controllers\JeffersonController;
+use Illuminate\Http\Request;
+use PHPUnit\Framework\TestCase;
 
 class JeffersonTest extends TestCase
 {
