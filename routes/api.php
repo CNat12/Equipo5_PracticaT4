effdbcb93c3cfa47ddf5000e9773e9bdadb84497
NuriX99 <nxdg785@gmail.com>
2025-12-07 18:12:00 -0600

    error corregido

--- routes/api.php
+++ routes/api.php
@@ -1,7 +1,6 @@
 <?php
 
-use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\NuriController;
-
+use Illuminate\Support\Facades\Route;
 
 Route::post('/math/analyze', [NuriController::class, 'analyze']);
